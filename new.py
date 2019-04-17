ff6323830b45628ee24f3be80e6d7ba8fa796772
jake-in-37cang <liujingjieyear@163.com>
2019-04-17 16:22:51 +0800

    从1个py导入另外1个py文件

--- new.py
+++ new.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
-
 # author:jakewendao
-
 from selenium import webdriver
 import unittest
 import sys
 
 class TestLogin(unittest.TestCase):
-
-    def setUpClass(self):
-        self.driver = webdriver.Firefox()
-    def tearDown(self):
-        self.driver.close()
-    def test_b(self):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+    def test_login3(self):
+        self.driver.get("https://www.v2ex.com/")
+        title = self.driver.title
+        print title
+    def test_login4(self):
         self.driver.get("http://www.baidu.com")
         title = self.driver.title
         print title
-    def test_a(self):
-        self.driver.get("http://www.baidu.com")
-        title = self.driver.title
-        print title*2
-if __name__ == '__main__':
+def login():
     # 需要先对TestSuite进行实例化
     # 注意TestLogin.test_a()也可以用TestLogin("test_a")来表达
-    suite = unittest.TestSuite()
-    suite.addTest(TestLogin("test_b"))
-    suite.addTest(TestLogin("test_a"))
-    # 然后执行这个套件
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    #这里为什么添加的先后顺序不行？
-    #2次提交
+    suite1 = unittest.TestSuite()
+    suite1.addTest(TestLogin('test_login4'))
+    suite1.addTest(TestLogin('test_login3'))
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite1)
---
+++ new2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# author:jakewendao
+
+
+a = new.login()
