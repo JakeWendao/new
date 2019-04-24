211c91d77a1165d7ae1e28daae4431ad082f66b2
JakeWendao <liujingjieyear@163.com>
2019-04-25 06:34:10 +0800

    增加新内容

--- new.py
+++ new.py
@@ -12,19 +12,16 @@ class TestLogin(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.driver.close()
-    def test_login3(self):
+    def test_login4(self):
         self.driver.get("https://www.v2ex.com/")
         title = self.driver.title
         print title
-    def test_login4(self):
+    def test_login3(self):
         self.driver.get("http://www.baidu.com")
         title = self.driver.title
         print title
 def login():
     # 需要先对TestSuite进行实例化
     # 注意TestLogin.test_a()也可以用TestLogin("test_a")来表达
-    suite1 = unittest.TestSuite()
-    suite1.addTest(TestLogin('test_login4'))
-    suite1.addTest(TestLogin('test_login3'))
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite1)
+    suite1 = unittest.makeSuite(TestLogin)
+    unittest.TextTestRunner(verbosity=2).run(suite1)
