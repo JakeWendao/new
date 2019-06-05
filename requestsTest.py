0caf9a2aae44d2050fca001a21595333f66589ed
jake-in-37cang <liujingjieyear@163.com>
2019-06-05 09:25:03 +0800

    requests学习，第一课。

--- requestsTest.py
+++ requestsTest.py
@@ -7,4 +7,4 @@ r = requests.get('http://www.baidu.com')
 print r.text
 print u'请求地址'+' ok ',r.url
 print r.status_code
-print r.json()   #这个地方报错是正常的，因为返回的信息不是json编码
+print r.json()   #这个地方报错是正常的，因为返回的信息不是json编码 1
