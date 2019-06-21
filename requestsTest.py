3495e191383a014ca58fb31c62c66175394dc533
jake-in-37cang <liujingjieyear@163.com>
2019-06-21 17:36:11 +0800

    中文乱码的解决办法。

--- requestsTest.py
+++ requestsTest.py
@@ -4,13 +4,17 @@
 
 import requests
 data = {'word':'Selenium自动化测试实战（Python语言版）'}
+data2 = {}
 
 r = requests.get('https://yuedu.baidu.com/search',params=data)
+r2 = requests.post('',data=data2,json=None)
+r2.text
 # r.encoding= 'iso8859-1'
+
+
 print u'请求地址'+' ok ',r.url
 
 print type(r)
-
 print r.text
 print r.json()
 print (r.text.encode(r.encoding).decode(r.apparent_encoding))
