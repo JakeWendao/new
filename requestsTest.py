03481f1f009a9b59d4749c5fbd0b9722ab864b05
jake-in-37cang <liujingjieyear@163.com>
2019-07-08 20:08:46 +0800

    增加post方法。

--- requestsTest.py
+++ requestsTest.py
@@ -3,18 +3,13 @@
 # author:jakewendao
 
 import requests
-data = {'word':'Selenium自动化测试实战（Python语言版）'}
-data2 = {}
+import json
+data2 = {'email':'liujingjieyear@163.com','icode':'','origURL':'http://www.renren.com/home','domain':'renren.com','key_id':'1','captcha_type':'web_login',
+         'password':'a63d66c01f190c64350e606173e0ff655972db4a16e12ef47dbeac1ec1cf1536','rkey':'45719fa80a9cb78dc16c3a4e87b896e9','f':''}
 
-r = requests.get('https://yuedu.baidu.com/search',params=data)
-r2 = requests.post('',data=data2,json=None)
-r2.text
-# r.encoding= 'iso8859-1'
-
+r = requests.post('http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=2019611953216',data=data2)
 
 print u'请求地址'+' ok ',r.url
-
-print type(r)
-print r.text
-print r.json()
-print (r.text.encode(r.encoding).decode(r.apparent_encoding))
+dic = json.loads(r.text.encode(r.encoding).decode(r.apparent_encoding))
+print dic
+print dic['failDescription']
